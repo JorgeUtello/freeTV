246134ccc1e33bda7db8a71180a277b4578f3c4e
jorgeutello <95353540+jorgeutello@users.noreply.github.com>
2025-12-06 08:53:11 -0300

    api/channels: add YouTube embedded channel (iframe)

--- api/channels.js
+++ api/channels.js
@@ -8,7 +8,14 @@ export default function handler(req, res) {
   const channels = [
     { id: 'america', name: 'America', proxy: true },
     { id: 'telefe', name: 'Telefe', proxy: true },
-    { id: 'eltrece', name: 'El Trece', proxy: true }
+    { id: 'eltrece', name: 'El Trece', proxy: true },
+    // Example of an embedded YouTube channel (iframe)
+    {
+      id: 'youtube_cb12KmMMDJA',
+      name: 'YouTube - Especial',
+      embed: true,
+      iframeUrl: 'https://www.youtube.com/embed/cb12KmMMDJA?si=TNIUnj3XPT9Owxm1'
+    }
   ];
 
   res.json({ channels });
