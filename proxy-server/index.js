4e4c9fe9b1b61120b8b5d7fa51725cc61f5d80ce
jorgeutello <95353540+jorgeutello@users.noreply.github.com>
2025-12-08 14:42:34 -0300

    Add initial HLS playlist for 720p streaming with segments

--- proxy-server/index.js
+++ proxy-server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const fetch = (...args) => import('node-fetch').then(m => m.default(...args));
+const fetch = require('node-fetch');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
